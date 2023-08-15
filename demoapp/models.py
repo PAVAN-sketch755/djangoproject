2dae7d5b17d847b993ad7c118efdf7961e3c2c2c
Pavan <potharajpavan7@gmail.com>
2023-08-15 23:04:35 +0530

    firstcommit

---
+++ demoapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from datetime import datetime    
+
+
+class Blog(models.Model):
+    title=models.CharField(max_length=100)
+    #images
+    image=models.ImageField(upload_to="images/")
+    
+    description=models.TextField()
+    author=models.CharField(max_length=100)
+    created_at=models.DateTimeField(default=datetime.now)
+    def __str__(self):
+        return self.title
